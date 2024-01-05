fa9267bdb3958d6be1ecb9306bca36f1d801012f
Nicola Colasanto <n.colasanto@studenti.uniba.it>
2024-01-05 19:11:22 +0100

    Add comments and better format print on console

--- src/eventBased/main/Main.java
+++ src/eventBased/main/Main.java
@@ -2,6 +2,7 @@ package eventBased.main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.TreeSet;
 
 import eventBased.model.Student;
@@ -12,12 +13,14 @@ import eventBased.view.StudentView;
 public class Main {
 
 	public static void main(String[] args) {
+		// Create students instance
 		Student stu1 = new Student("Alex", 22, Student.Course.MATH);
-		Student stu2 = new Student("Bob", 15, Student.Course.LECTURE);
+		Student stu2 = new Student("Bob", 15, Student.Course.LITERATURE);
 		Student stu3 = new Student("Truce", 12, Student.Course.MATH);
 		Student stu4 = new Student("Susy", 18, Student.Course.ITPS);
 		Student stu5 = new Student("Baldrog", 18, Student.Course.MATH);
 		
+		// Create views for each student
 		StudentView stuView1 = new StudentView(stu1);
 		StudentView stuView2 = new StudentView(stu2);
 		StudentView stuView3 = new StudentView(stu3);
@@ -27,22 +30,41 @@ public class Main {
 		var studentList = new ArrayList<Student>(List.of(stu5, stu3, stu2, stu4,stu1));
 		var studentViewList = new ArrayList<StudentView>(List.of(stuView1, stuView2, stuView3, stuView4,stuView5));
 		
+		// Print view for each student
 		for(StudentView stuV : studentViewList) {
 			stuV.printStudent();
 		}
 		
+		// Create classroom with four students
 		ClassroomView classroomView = new ClassroomView("science", studentList);
 		classroomView.removeStudent(stu3);
+		// Print classroom
 		classroomView.printClassroom();
 		
-		System.out.println("NOW I WILL CHANGE FOLLOWING STUDENT ATTRIBUTES");
-		stu3.setCourse(Course.ITPS);
-		stu4.setCourse(Course.LECTURE);
+		printWithFormat("EVENT PATTERN IN ACTION", null);
 		
-		System.out.println("NOW STUDENTS WILL NOT STUDENT ANYMORE");
-		stu1.noActiveStudent();
-		stu5.noActiveStudent();
-		stu3.noActiveStudent();
+		printWithFormat("NOW I WILL CHANGE FOLLOWING STUDENT ATTRIBUTES", () -> {
+			stu3.setCourse(Course.ITPS);
+			stu4.setCourse(Course.LITERATURE);
+		});
+		
+		printWithFormat("NOW STUDENTS WILL NOT STUDENT ANYMORE", () -> {
+			stu1.noActiveStudent();
+			stu5.noActiveStudent();
+			stu3.noActiveStudent();
+		});
+	}
+	
+	public static void printWithFormat(String title, Runnable function) {
+		String separatorChar = "_";
+		int howManyTimes = 200;
+		System.out.println(separatorChar.repeat(howManyTimes));
+		System.out.println("/* %s */".formatted(title.toUpperCase()));
+		System.out.println(separatorChar.repeat(howManyTimes));
+		if(Objects.nonNull(function)) {
+			function.run();
+			System.out.println(separatorChar.repeat(howManyTimes));
+		}
 	}
 
 }
